77d09dd53777ace7c75519725a6e8bbc12556004
Student 47 <student47@Codes-MacBook-Air-3.local>
2024-01-25 14:12:21 -0500

    not completed yet

---
+++ quiz-online-client/utils/QuizService.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const api = axios.create({
+baseURL: "http://localhost:9192/api/quizzes"
+})
+
+export const createQuestion = async(quizQuestion) =>{
+    try {
+        catch (error){}
+    }
+}
